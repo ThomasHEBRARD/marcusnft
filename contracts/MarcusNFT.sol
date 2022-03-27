6f6a6d391323e9d9b6ddca226b88929344ebbb14
thebrard <thomas.hebrard134@gmail.com>
2022-03-27 17:56:16 +0200

    changes

--- contracts/MarcusNFT.sol
+++ contracts/MarcusNFT.sol
@@ -12,7 +12,7 @@ contract MarcusNFT is ERC721Enumerable, Ownable {
     string public baseExtension = ".json";
     uint256 public cost = 0.05 ether;
     uint256 public presaleCost = 0.03 ether;
-    uint256 public maxSupply = 100000;
+    uint256 public maxSupply = 1000;
     uint256 public maxMintAmount = 2;
     bool public paused = false;
     mapping(address => bool) public whitelisted;
@@ -24,7 +24,7 @@ contract MarcusNFT is ERC721Enumerable, Ownable {
         string memory _initBaseURI
     ) ERC721(_name, _symbol) {
         setBaseURI(_initBaseURI);
-        mint(msg.sender, 1000);
+        mint(msg.sender, 2);
     }
 
     // internal
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,19 @@
-require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
-  defaultNetwork: "matic",
   networks: {
-    hardhat: {},
-    matic: {
-      url: "https://polygon-mumbai.infura.io/v3/a39885f9f4ce4e58a20fbc85c2a3b794",
-      accounts: [
-        "73d0f0398b2543e84b29c173dca222333fc44a835d523f31c1067dca4a7b25b6",
-      ],
+    mumbai: {
+      url: process.env.RPC,
+      accounts: [process.env.PRIVATE_KEY],
       gas: 2100000,
-      gasPrice: 8000000000,
-    },
+      gasPrice: 8000000000
+    }
   },
   solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
+    version: "0.8.4",
+  },
+  paths: {
+    sources: './contracts',
   },
 };
